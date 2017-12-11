bd002dee24a62d9e467f3d601a8a83f43885dea7
Ivanov Sergey <djapy@yandex.ru>
2017-12-11 16:43:52 +0300

    Create initial commit for receiving json

---
+++ realty_app/proc_json.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import argparse
+
+from .set  import db
+from .models import Real_estate
+
+
+url = 'https://devman.org/media/filer_public/e5/62/' \
+      'e56287d2-9519-4e18-878a-6d4849b628e2/ads.json'
+
+
+def load_json(url):
+    json = requests.get(url)
+    json_content = json.json()
+    print(json_content)
+    return json_content
+
+
+def get_json_from_unix(file):
+    return json.load(file)
+
+
+def add_real_estate_content(json_content):
+    for apartment in json_content:
+        apartment_data = Real_estate(id=apartment['id'],
+                                     settlement=apartment['settlement'],
+                                     under_construction=apartment['under_construction'],
+                                     description=apartment['description'],
+                                     price=apartment['price'],
+                                     oblast_district=apartment['oblast_district'],
+                                     living_area=apartment['living_area'],
+                                     has_balcony=apartment['has_balcony'],
+                                     address=apartment['address'],
+                                     construction_year=apartment['construction_year'],
+                                     rooms_number=apartment['rooms_number'],
+                                     premise_area=apartment['premise_area'],
+                                     active=True)
+        db.session.add(apartment_data)
+        db.session.commit()
+
+
+
+def parser_for_update_db():
+    parser = argparse.ArgumentParser(description="Updates the database")
+    parser.add_argument('-u', '--update', required=True,
+                        type=argparse.FileType(mode='r'),
+                        help='Allows you to update the database'
+                             ' from the upload, in json format')
+
+
+if __name__ == '__main__':
+
+
+    json_content = load_json(url)
+    add_real_estate_content(json_content)
