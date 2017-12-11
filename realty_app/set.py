696b19be10e70194f4ee6634c54b47e72039d865
Ivanov Sergey <djapy@yandex.ru>
2017-12-11 16:42:02 +0300

    Create an application

---
+++ realty_app/set.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from .models import db
+
+
+app = Flask(__name__)
+app.config.from_object('config')
+db.app = app
+db.init_app(app)
+
+if not os.path.exists('real_estate.db'):
+    db.create_all()
+
+from .views import add_real_estate_content
